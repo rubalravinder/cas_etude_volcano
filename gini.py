9a75f4f954c2b2ebcac608751073eb15bd52a101
SylvanBrocard <sylvan.brocard@gmail.com>
2021-11-18 11:22:26 +0100

    added rand score and other algos

---
+++ gini.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def gini(x):
+    # (Warning: This is a concise implementation, but it is O(n**2)
+    # in time and memory, where n = len(x).  *Don't* pass in huge
+    # samples!)
+
+    # Mean absolute difference
+    mad = np.abs(np.subtract.outer(x, x)).mean()
+    # Relative mean absolute difference
+    rmad = mad/np.mean(x)
+    # Gini coefficient
+    g = 0.5 * rmad
+    return g
